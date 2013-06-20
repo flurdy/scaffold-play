ddda2cad257a505793699b8e1c7f417b9f2548f4
ivar <ivar@flurdy.com>
2013-06-20 01:44:57 +0100

    authentication

--- app/models/Domain.scala
+++ app/models/Domain.scala
@@ -10,37 +10,52 @@ case class ApplicationUser(userId: Option[String], username: String,
 				 fullname: Option[String]) = this(None,username,email,fullname)
 }
 
-case class UserCredentials(user: ApplicationUser, password: Option[String]){
-	def this(user: ApplicationUser) = this(user,None)
+case class UserCredentials(user: ApplicationUser, password: Option[String], permission: Option[Permission]){
+	def this(user: ApplicationUser) = this( user, None, None)
 	def register = UserCredentials.register(this)
 }
+
+sealed trait Permission
+case object Administrator extends Permission
+case object NormalUser extends Permission
+case object Anyone extends Permission
 
 object UserCredentials {
 
 	def authenticate(username: String, enteredPassword: String) = {
-		ApplicationUser.findByUsername(username) map { applicationUser =>
-			applicationUser.userId map { userId =>
-				findCredentials(userId) map { hashedPassword =>
-					if(BCrypt.checkpw(enteredPassword,hashedPassword)){
-						Logger.info(s"Authenticated $username")
-						Some(applicationUser)
-					} else None
-				}
+		ApplicationUser.findByUsername(username) flatMap { applicationUser =>
+			findCredentials(applicationUser) flatMap { hashedPassword =>
+				if(BCrypt.checkpw(enteredPassword, hashedPassword)){
+					Logger.info(s"Authenticated $username")
+					findByApplicationUser(applicationUser)
+				} else None
 			}
 		}
 	}
 
-	private def findCredentials(userId: String): Option[String] = {
+
+	def findById(userId: String): Option[UserCredentials] = ApplicationUser.findById(userId) flatMap ( findByApplicationUser(_) )
+
+	def findByApplicationUser(applicationUser: ApplicationUser): Option[UserCredentials] = {
+		applicationUser.userId flatMap { userId =>
+//			 map { permission =>
+			Some(UserCredentials(applicationUser, None, Some(NormalUser)))
+//			}
+		}
+	}
+
+	private def findCredentials(applicationUser: ApplicationUser): Option[String] = {
 		// TODO Lookup credentials in persistance store
 		None
 	}
 
-	def register(userCredentials: UserCredentials) {
+	def register(userCredentials: UserCredentials): String = {
 		// TODO store registration in persistance store
 
 		userCredentials.password map { password =>
 			val encryptedPassword = encrypt(password)
 		}
+		"123"
 	}
 
 	private def encrypt(password: String): String = {
@@ -57,6 +72,14 @@ object ApplicationUser {
 	def findByUsername(username: String): Option[ApplicationUser] = {
 		// TODO find application user in persistance store
 		None
+	}
+
+
+	def findById(userId: String): Option[ApplicationUser] = {
+		val username = ""
+		val email = ""
+		val fullname = ""
+		Some(ApplicationUser(Some(userId),username,Some(email),Some(fullname)))
 	}
 
 }
--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,9 @@ object ApplicationBuild extends Build {
     "org.webjars" % "webjars-play" % "2.1.0",
     "org.webjars" % "jquery" % "1.9.1",
     "org.webjars" % "bootstrap" % "2.3.0",
-    "org.mindrot" % "jbcrypt" % "0.3m"
+    "org.mindrot" % "jbcrypt" % "0.3m",
+    "jp.t2v" %% "play2.auth"      % "0.9",
+    "jp.t2v" %% "play2.auth.test" % "0.9" % "test"
   )
 
 
